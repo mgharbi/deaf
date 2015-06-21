b83c68d5c2ffb9e0a80546f03916854adcbdb87a
Jimmy Ren <jimmy.sj.ren@gmail.com>
2015-06-21 16:08:29 +0800

    Added images and utility files for deaf.

--- applications/deep_edge_aware_filters/utility/prepare_net_filter.m
+++ applications/deep_edge_aware_filters/utility/prepare_net_filter.m
@@ -1,34 +1,38 @@
-function prepare_net(input_x, input_y, w_path)
-    global config;
+function prepare_net_filter(input_x, input_y, w_path)
+    global config mem;
     if(input_x < input_y)
         smaller_edge = input_x;
     else
         smaller_edge = input_y;
     end
 
-    max_patch_size = 120;
-    %max_patch_size = 100;
+    max_patch_size = 270;
 
     if(smaller_edge <= max_patch_size)
         p_size = smaller_edge;
     else
         p_size = max_patch_size;
     end
-
-    load(w_path);
-    config = model;
-    config.batch_size = 1;
-    config.normalize_init_weights = 0;  % for old version weights
-    size_differ = [config.input_size(1)-config.output_size(1) config.input_size(2)-config.output_size(2)];
-    config.input_size = [p_size p_size];
-    config.output_size = [p_size-size_differ(1) p_size-size_differ(2) config.chs];
-    config.size_differ = size_differ;
     
-    init(1);
     load(w_path);
+    persistent init_called;
+    if(isempty(init_called))
+        config = model;
+        config.batch_size = 2;
+        size_differ = [config.input_size(1)-config.output_size(1) config.input_size(2)-config.output_size(2)];
+        config.input_size = [p_size p_size];
+        config.output_size = [p_size-size_differ(1) p_size-size_differ(2) config.chs];
+        config.size_differ = size_differ;
+        init(1);
+        load(w_path);
+        config.SCALE_INPUT = model.SCALE_INPUT;
+        config.SCALE_OUTPUT = model.SCALE_OUTPUT;
+        init_called = 1;
+    end
+    
     config.weights = model.weights;
 
-    overlap_pixels = 28;
+    overlap_pixels = 30;
     problem_pixels = 0;
     
     start_rows = [1];
@@ -61,4 +65,5 @@ function prepare_net(input_x, input_y, w_path)
     config.MEM.p_size = p_size;
     config.MEM.start_rows = start_rows;
     config.MEM.start_cols = start_cols;
+    mem.fake_output_for_test = config.NEW_MEM(zeros(config.output_size(1), config.output_size(2), config.output_size(3), config.batch_size));
 end
